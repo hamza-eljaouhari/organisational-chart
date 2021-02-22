c883f7cd4dc637acba082d7384dcfff72cd9edff
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2021-02-22 21:23:38 +0100

    Switching yo MySQL

--- config.php
+++ config.php
@@ -1,3 +1,17 @@
 <?php
 
 // Server=localhost;Database=master;Trusted_Connection=True;
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$database = "organizational_chart";
+
+try {
+  $conn = new PDO("sqlsrv:host=$servername;dbname=master    ", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
--- tables.sql
+++ tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE node_tree
 (
     idNode INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    [level] INT,
+    level INT,
     iLeft INT,
     iRight INT
-)
+);
 
 CREATE TABLE node_tree_names
 (
     idNode INT NOT NULL,
-    [language] VARCHAR(15),
+    language VARCHAR(15),
     nodeName VARCHAR(255),
     FOREIGN KEY (idNode) REFERENCES node_tree(idNode)
-)
+);
